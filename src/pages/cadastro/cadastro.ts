540ea82e023df5882d473ffa29302ad7c6ae84e9
Johann Lucas <johann.lucas@hotmail.com>
2018-08-21 22:37:57 -0300

    cadastro page implementations #1

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -9,13 +9,25 @@ import { Carro } from '../../app/models/carro';
 })
 export class CadastroPage {
 
-  public carro:  Carro;
+  public carro: Carro;
   public precoTotal: number;
+
+  public nome: string = '';
+  public endereco: string = '';
+  public email: string = '';
+  public data: string = new Date().toISOString();
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams) {
 
-      this.carro = this.navParams.get('carroSelecionado');
-      this.precoTotal = this.navParams.get('precoTotal');
+    this.carro = this.navParams.get('carroSelecionado');
+    this.precoTotal = this.navParams.get('precoTotal');
+  }
+
+  agenda() {
+    console.log(this.nome);
+    console.log(this.endereco);
+    console.log(this.email);
+    console.log(this.data);
   }
 }
