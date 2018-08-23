ee89546579671f1915cf98854f8d20193b12552c
Johann Lucas <johann.lucas@hotmail.com>
2018-08-22 23:30:46 -0300

    agendamentos provider

---
+++ src/providers/agendamentos-service/agendamentos-service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class AgendamentosServiceProvider {
+
+  private _url = 'http://localhost:8080/api';
+
+  constructor(public _http: Http) {
+  }
+
+  agenda(agendamento) {
+    return this._http.post(this._url + '/agendamento/agenda', agendamento)
+  }
+}
