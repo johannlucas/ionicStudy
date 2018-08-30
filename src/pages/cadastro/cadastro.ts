310bb85bad9b6225370fba74edf83c02c874647e
Johann Lucas <johann.lucas@hotmail.com>
2018-08-29 23:43:02 -0300

    cadastro page alert

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, Alert } from 'ionic-angular';
 import { Carro } from '../../app/models/carro';
 import { AgendamentosServiceProvider } from '../../providers/agendamentos-service/agendamentos-service';
 
@@ -17,21 +17,18 @@ export class CadastroPage {
   public endereco: string = '';
   public email: string = '';
   public data: string = new Date().toISOString();
+  public _alerta: Alert;
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
-    private _agendamentosService: AgendamentosServiceProvider) {
+    private _agendamentosService: AgendamentosServiceProvider,
+    private _alertCtrl: AlertController) {
 
     this.carro = this.navParams.get('carroSelecionado');
     this.precoTotal = this.navParams.get('precoTotal');
   }
 
   agenda() {
-    console.log(this.nome);
-    console.log(this.endereco);
-    console.log(this.email);
-    console.log(this.data);
-
     let agendamento = {
       nomeCliente: this.nome,
       enderecoCliente: this.endereco,
@@ -40,10 +37,23 @@ export class CadastroPage {
       precoTotal: this.carro.preco
     }
 
+    this._alerta = this._alertCtrl.create({
+      title: 'Aviso',
+      buttons: [
+        { text: 'Ok'}
+      ]
+    });
+
     this._agendamentosService.agenda(agendamento)
       .subscribe(
-        () => alert('Agendou!'),
-        () => alert('Deu problema!')
+        () => {
+          this._alerta.setSubTitle('Agendamento realizado!');
+          this._alerta.present();
+        },
+        () => {
+          this._alerta.setSubTitle('Algum erro aconteceu!');
+          this._alerta.present();
+        }
       );
   }
 }
